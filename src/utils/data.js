100ef6694998be79eb45178b19f7f52e2ae91856
Ahmad Mustafa <ahmad786naushahi@gmail.com>
2025-11-13 18:47:39 +0500

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+export const navItems = [
+        {Id:'1', link:'/articles', title:'Articles'},
+        {Id:'2', link:'/sports', title:'Sports'},
+        {Id:'3', link:'/music', title:'Music'},
+        {Id:'4', link:'/art', title:'Art'},
+        {Id:'5', link:'/history', title:'History'}
+]
+
+export const newsData = [
+    {
+        "id": 1,
+        "title": "New ES6 upgrade available",
+        "feed": "The query 'New ES6 upgrade available' is ambiguous, as 'ES6' can refer to the JavaScript standard or a Chinese electric vehicle model. If you mean the JavaScript standard, there are no 'new' ES6 upgrades; ES6 is a standard that was released in 2015, and it has been followed by annual updates like ES2016, ES2017, etc. "
+    },
+    {
+        "id": 2,
+        "title": "The importance of REACT in the development",
+        "feed": "The answer to why use React. js over other frameworks lies in its simplicity, performance, and flexibility. Its virtual DOM implementation ensures efficient updates, faster rendering, and better performance compared to traditional DOM manipulations"
+    },
+    {
+        "id": 3,
+        "title": "REACT developers going on strike",
+        "feed": "There is no evidence of a global React developer strike; the term likely refers to online discussions about frustrations within the developer community. These discussions are about the evolving job market, the impact of AI, changes to tooling like Create React App, and a general sense of fragmentation in the ecosystem. "
+    },
+    {
+        "id": 4,
+        "title": "Say what again. I dare you.",
+        "feed": "The word meme was coined by Richard Dawkins in his 1976 book The Selfish Gene as an attempt to explain how aspects of culture replicate, mutate, and evolve (memetics). Emoticons are among the earliest examples of internet memes, specifically the smiley emoticon ':-)', introduced by Scott Fahlman in 1982."
+    }
+]
